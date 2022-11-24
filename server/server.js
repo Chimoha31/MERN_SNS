91373b0e4b3dcacabb131570305a66b793307180
Chimoha31 <mokochii1108@gmail.com>
2022-11-23 16:12:23 -0800

    Connected mongodb

--- server/server.js
+++ server/server.js
@@ -1,5 +1,32 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
 const PORT = 8000;
+
+// Test
+app.get("/", (req, res) => {
+  res.send("This is endpoint");
+});
+
+// From router
+const authRouter = require("./router/auth");
+const userRouter = require("./router/users");
+const postRouter = require("./router/posts");
+
+// Connect MongoDB
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Succesfully connected to MongoDB 🚀");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+// Middleware
+app.use("/api/auth", authRouter);
+app.use("/api/users", userRouter);
+app.use("/api/posts", postRouter);
 
 app.listen(PORT, () => console.log("Server is running 😎"));
