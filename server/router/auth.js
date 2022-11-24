38ed64f0addf6ac83469b64b00741eadfc21a78d
Chimoha31 <mokochii1108@gmail.com>
2022-11-23 22:13:57 -0800

    Login update user

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/Users");
 
+// Resister
 router.post("/resister", async (req, res) => {
   try {
     const newUser = await new User({
@@ -16,8 +17,20 @@ router.post("/resister", async (req, res) => {
   }
 });
 
-// router.get("/", (req, res) => {
-//   res.send("auth endpoint");
-// });
+// Login
+router.post("/login", async (req, res) => {
+  try {
+    // mongooseにあるfindOne()で探してくるfinOne()内に記載した情報を持っているUserを探しにいく
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(404).send("There is no user...");
+
+    // Line23-24でemailが一致しているか確認が取れたら、Line27-29が走りパスワードが一致してるか確認しにいく
+    const validPassword = req.body.password === user.password;
+    if (!validPassword) return res.status(400).json("Not match your password");
+    return res.status(200).json(user);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
 
 module.exports = router;
--- server/router/users.js
+++ server/router/users.js
@@ -1,9 +1,27 @@
 const router = require("express").Router();
 const User = require("../models/Users");
 
+// Get
 
-// router.get("/", (req, res) => {
-//   res.send("users endpoint");
-// });
+// Update
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+        // $set=全てのパラメーター。ここではmodelsの全てのUser model。それをpostmanで打つreq.bodyで書き換える。
+      });
+      return res.status(200).json("Succesfully updated user information!");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res
+      .status(403)
+      .json("You can update information only on your account");
+  }
+});
+
+// Delete
 
 module.exports = router;
