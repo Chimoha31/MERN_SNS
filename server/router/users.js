75a58908cb6fa26a1afbf76d584616cd97641c90
Chimoha31 <mokochii1108@gmail.com>
2022-11-24 18:10:47 -0800

    Get & dekete user

--- server/router/users.js
+++ server/router/users.js
@@ -2,6 +2,16 @@ const router = require("express").Router();
 const User = require("../models/Users");
 
 // Get
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    // passwordとupdatedAtは第三者に見られない様に下記のように取り出し、分割代入のothersだけ取得する様にする
+    const {password, updatedAt, ...others} = user._doc;
+    return res.status(200).json(others);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
 
 // Update
 router.put("/:id", async (req, res) => {
@@ -18,10 +28,24 @@ router.put("/:id", async (req, res) => {
   } else {
     return res
       .status(403)
-      .json("You can update information only on your account");
+      .json("You can update the information only on your account");
   }
 });
 
 // Delete
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+      return res.status(200).json("Succesfully deleted the information");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res
+      .status(403)
+      .json("You can delete the nformation only on your account");
+  }
+});
 
 module.exports = router;
