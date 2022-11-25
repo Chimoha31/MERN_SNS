b58bf84223c6cbd5bc35cffeb598ba7cd843fb66
Chimoha31 <mokochii1108@gmail.com>
2022-11-24 20:55:31 -0800

    Get Update Delete

--- server/router/posts.js
+++ server/router/posts.js
@@ -13,6 +13,49 @@ router.post("/", async (req, res) => {
   }
 });
 
+// Get a single Post
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    return res.status(200).json(post);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
 // Update Post
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({
+        $set: req.body,
+      });
+      return res.status(200).json("Successfully updated your post");
+    } else {
+      return res.status(403).json("You can't edit someone's post.");
+    }
+  } catch (err) {
+    return res.status(403).json(err);
+  }
+});
+
+// Delete Post
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.deleteOne();
+      return res.status(200).json("Succesfully deleted post!");
+    } else {
+      return res.status(403).json("You can't delete someone's post.");
+    }
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+// Press a particular likes
+
 
 module.exports = router;
