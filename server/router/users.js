960fbba69e348ee25eaf7a1b41a2e0ffb975fd5e
Chimoha31 <mokochii1108@gmail.com>
2022-11-24 19:07:05 -0800

    Follow api

--- server/router/users.js
+++ server/router/users.js
@@ -6,7 +6,7 @@ router.get("/:id", async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
     // passwordとupdatedAtは第三者に見られない様に下記のように取り出し、分割代入のothersだけ取得する様にする
-    const {password, updatedAt, ...others} = user._doc;
+    const { password, updatedAt, ...others } = user._doc;
     return res.status(200).json(others);
   } catch (err) {
     return res.status(500).json(err);
@@ -45,6 +45,36 @@ router.delete("/:id", async (req, res) => {
     return res
       .status(403)
       .json("You can delete the nformation only on your account");
+  }
+});
+
+// Follow a user
+router.put("/:id/follow", async (req, res) => {
+  // userIdがparamsのidではない時＝自分のIdでない時＝相手のId(自分以外をfollowする)
+  if (req.bpdy.userId !== req.params.id) {
+    // followする相手のIdを探しにく
+    const user = await User.findById(req.params.id);
+    // 自分のId
+    const currentUser = await User.findById(req.body.userId);
+    // followする相手のfollowersの中に自分のIdが無かったらフォルーできる
+    if (!user.followers.includes(req.body.userId)) {
+      await user.updateOne({
+        $push: {
+          followers: req.body.userId,
+        },
+      });
+      //自分のfollowingsも追加する
+      await currentUser.updateOne({
+        $push: {
+          followings: req.params.id,
+        },
+      });
+      return res.status(200).json("Succesfully followed");
+    } else {
+      return res.status(403).json("You already followed this user");
+    }
+  } else {
+    return res.status(500).json("You can't follow yourself");
   }
 });
 
